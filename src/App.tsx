b4c5bb340afe4b03fe24e1804e774ea94a106f33
Kiril <kirbou06012@gmail.com>
2026-02-01 11:56:53 -0400

    added glowing sun

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import './App.css'
-import {Suspense, useRef, useState} from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import Sun from './components/models/Sun.jsx'
-import type {ThreeElements} from '@react-three/fiber'
-import * as THREE from 'three'
-import {Stage} from "@react-three/drei";
+import { Canvas } from '@react-three/fiber'
+import GlowingSun from "./components/GlowingSun.tsx";
+
 
 // 2. Export a new App component that renders the Canvas
 export default function App() {
   return (
     <div style={{ height: '100vh', width: '100vw' }}>
-      <Canvas>
-        {/*<ambientLight intensity={Math.PI / 2} />*/}
-        {/*<spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />*/}
-
-          <Suspense fallback={null}>
-              <Stage intensity={0.1}>
-                  <Sun />
-              </Stage>
-          </Suspense>
+      <Canvas dpr={[1, 1]}>
+          <GlowingSun />
+          <ambientLight intensity={0.5} />
       </Canvas>
     </div>
   )
