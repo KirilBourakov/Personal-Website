be89c242c0e52f9321abe2c77aee17bbe0eae5d2
KirilBourakov <kiril.bourakov@smartskintech.com>
2026-02-03 11:57:08 -0400

    added first version of label

--- src/components/SunGroup.tsx
+++ src/components/SunGroup.tsx
@@ -3,6 +3,7 @@ import {useFrame} from "@react-three/fiber";
 import type {Mesh} from "three";
 import {useRef} from "react";
 import Sun from "./models/Sun.tsx";
+import {Html, Line} from "@react-three/drei";
 
 export default function SunGroup(){
     const meshRef = useRef<Mesh>(null!)
@@ -13,6 +14,17 @@ export default function SunGroup(){
     });
 
     return (
-        <Sun scale={.25} ref={meshRef}/>
+        <>
+            <Sun scale={.25} ref={meshRef}/>
+            <Line points={[[0, 0, 0], [0, -1.2, 0], [0.5, -1.2, 0]]} color="black" opacity={.3} lineWidth={2} />
+            <Html position={[0.5, -1, 0]}>
+                <div
+                    className="p-2 rounded-lg whitespace-nowrap bg-black/30 text-white"
+                >
+                    [ RESUME ]
+                </div>
+            </Html>
+        </>
+
     )
 }
