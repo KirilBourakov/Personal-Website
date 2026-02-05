4a0774bbbaff7acb2271904eb80ba90c3b579d40
KirilBourakov <kiril.bourakov@smartskintech.com>
2026-02-05 11:29:45 -0400

    added grow on hover

--- src/components/SunGroup.tsx
+++ src/components/SunGroup.tsx
@@ -1,10 +1,11 @@
 import {useFrame} from "@react-three/fiber";
 import type {Mesh} from "three";
-import {useRef} from "react";
+import {useRef, useState} from "react";
 import Sun from "./models/Sun.tsx";
 import {Html, Line} from "@react-three/drei";
 
 export default function SunGroup(){
+    const [hover, setHover] = useState(false);
     const meshRef = useRef<Mesh>(null!)
     useFrame((_state, delta) => {
         if (meshRef.current) {
@@ -14,15 +15,27 @@ export default function SunGroup(){
 
     return (
         <>
-            <Sun scale={.25} ref={meshRef}/>
-            <Line points={[[0, 0, 0], [0, -1.2, 0], [0.5, -1.2, 0]]} color="black" opacity={.3} lineWidth={2} />
-            <Html position={[0.5, -1, 0]}>
-                <div
-                    className="p-2 rounded-lg whitespace-nowrap bg-black/30 text-white"
-                >
-                    [ RESUME ]
-                </div>
-            </Html>
+            <group
+                onPointerOver={() => setHover(true)}
+                onPointerOut={() => setHover(false)}
+            >
+                <Sun scale={hover ? .3 : .25} ref={meshRef}/>
+                <Line
+                    points={[[0, 0, 0], [0, -1.2, 0], [0.5, -1.2, 0]]}
+                    color="black"
+                    opacity={.3}
+                    lineWidth={hover ? 3 : 2}
+                />
+                <Html position={[0.5, -1, 0]}>
+                    <div
+                        onMouseEnter={() => setHover(true)}
+                        onMouseLeave={() => setHover(false)}
+                        className={`${hover ? "p-3" : "p-2"} rounded-lg whitespace-nowrap bg-black/30 text-white`}
+                    >
+                        [ RESUME ]
+                    </div>
+                </Html>
+            </group>
         </>
 
     )
